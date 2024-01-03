ed491459cfbacdb4a6cfbe5270e0f7466027049a
Erik Lopes de Oliveira <erik.lopesoliveira2019@gmail.com>
2024-01-03 17:21:16 -0300

    Fix linting issues and update Button component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,11 @@
-"use client"
-import Button from '@/components/Button'
-import React from 'react'
+'use client';
+import Button from '@/components/Button';
+import React from 'react';
 
 function Admin() {
   return (
-    <Button text="Admin" className="bg-slate-500" onClick={() => alert("test")} />
-  )
+    <Button text="Admin" type="button" className="bg-slate-500" onClick={() => alert('test')} />
+  );
 }
 
-export default Admin
+export default Admin;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,26 @@
-"use client";
-import React, { ComponentProps } from 'react'
-import { VariantProps, tv } from 'tailwind-variants'
+'use client';
+import React, { ComponentProps } from 'react';
+import { VariantProps, tv } from 'tailwind-variants';
 
 const button = tv({
-    base: ' text-white font-bold py-2 px-4 rounded',
-    variants: {
-        types: {
-            primary: 'bg-blue-500',
-            secondary: 'bg-gray-500',
-        }
-    },
-    defaultVariants: {
-        types: 'primary'
+  base: 'text-white font-bold py-2 px-4 rounded',
+  variants: {
+    schema: {
+      primary: 'bg-blue-500',
+      secondary: 'bg-gray-500',
     }
-})
+  },
+  defaultVariants: {
+    schema: 'primary'
+  }
+});
 
 type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button> & {
-    text: string
+  text: string
 } 
 
-export default function Button({ text, types, className, ...props }: ButtonProps) {
+export default function Button({ text, schema, className, ...props }: ButtonProps) {
   return (
-    <button className={button( { types, className } )} {...props}>{text}</button>
-  )
+    <button className={button( { schema, className } )} {...props}>{text}</button>
+  );
 }
